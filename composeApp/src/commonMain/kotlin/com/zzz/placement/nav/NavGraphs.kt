8c32cc548260ee5b40234cdd7be3f913a45aec6a
Atharva Pajgade <atharvapajgade2004@gmail.com>
2026-04-05 16:12:52 +0530

    some changes

--- composeApp/src/commonMain/kotlin/com/zzz/placement/nav/NavGraphs.kt
+++ composeApp/src/commonMain/kotlin/com/zzz/placement/nav/NavGraphs.kt
@@ -5,8 +5,8 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
-import com.zzz.feature.auth.SignUpScreen
 import com.zzz.feature.auth.login.LoginScreen
+import com.zzz.feature.auth.signup.SignUpScreen
 
 fun NavGraphBuilder.authGraph(
     navController: NavController
